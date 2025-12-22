b886d7f2149ccdf330eed82215dc65ae0db750f4
00-11-00 <shishirbartaula671@gmail.com>
2025-12-22 11:36:28 +0545

    Add Relationships between model

--- chai/admin.py
+++ chai/admin.py
@@ -1,8 +1,30 @@
 from django.contrib import admin
 #This code registers a Django model with the Django admin panel
 #  so it can be managed through the admin interface.
+from .models import chaiVarity,chaiReview,store,chaiCertification
+ #Imports the  chaiVariy model class name from models.py
 
-from .models import chaiVarity  #Imports the  chaiVariy model class name from models.py
-admin.site.register(chaiVarity)  #Registers the chaiVariy model with Django Admin
+# admin.site.register(chaiVarity)  #Registers the chaiVariy model with Django Admin
 
-# Register your models here.
+class ChaiReviewInline(admin.TabularInline):
+    model = chaiReview
+    extra = 2  #Number of extra forms to display
+
+class ChaiVarityAdmin(admin.ModelAdmin):
+    inlines = [ChaiReviewInline]
+    list_display = ('name', 'type', 'price', 'date_added','image','rating')
+    search_fields = ('name', 'type')
+    list_filter = ('type', 'date_added')
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location', 'opening_hours', 'contact_info')
+    search_fields = ('name', 'location')
+    filter_horizontal = ('chai_varieties',)
+
+class ChaiCertificationAdmin(admin.ModelAdmin):
+    list_display = ('chai_certification','certificate_number', 'issued_date', 'valid_until')   
+
+
+admin.site.register(chaiVarity, ChaiVarityAdmin)   
+admin.site.register(store, StoreAdmin)
+admin.site.register(chaiCertification, ChaiCertificationAdmin)
--- chai/models.py
+++ chai/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone #to add time zone 
-
+from django.contrib.auth.models import User #to link reviews to users
 
 # Create your models here.
 
@@ -21,10 +21,56 @@ class chaiVarity(models.Model):
 
     date_added=models.DateTimeField(default=timezone.now)
     type=models.CharField(max_length=20, choices=CHAI_TYPE_CHOICES)
+    rating = models.PositiveIntegerField(null=True)  #average rating based on user reviews
     description=models.TextField(default='')
-
-
 
     def __str__(self):
         return self.name  #When you print an instance of this model, 
     #it will return the name of the tea variety.otherwise it will show chaiVarity object (1) etc.
+
+
+#review model to store user reviews for each chai variety
+#one-to-many relationship between chaiVarity and chiaReview
+class chaiReview(models.Model):
+    chai_variety = models.ForeignKey(chaiVarity, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.PositiveIntegerField()  #assuming rating is from 1 to 5
+    comment = models.TextField()
+    date_created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'Review for {self.chai_variety.name} by {self.user.username}'
+
+
+   #store model to represent physical or online stores selling chai varieties
+   # many-to-many relationship between chaiVarity and store 
+    
+class store(models.Model):   
+     name = models.CharField(max_length=100)
+     location = models.CharField(max_length=255)
+     chai_varieties = models.ManyToManyField(chaiVarity, related_name='stores')  
+     opening_hours = models.CharField(max_length=100)
+     contact_info = models.CharField(max_length=100)
+
+     def __str__(self):
+            return self.name
+        
+
+       #model to represent certifications for chai varieties
+       # one-to-one relationship between chaiVarity and chaiCertification 
+
+class chaiCertification(models.Model):
+    chai_certification=models.OneToOneField(chaiVarity, on_delete=models.CASCADE,
+                                                 related_name='certification')
+    certificate_number = models.CharField(max_length=100)
+    issued_date = models.DateTimeField(default=timezone.now)
+    valid_until = models.DateField()
+    certification_body = models.CharField(max_length=100,null=True)
+
+
+    def __str__(self):
+            return f'{self.certification_body}certification for {self.chai_certification.name}'   
+
+    
+
+
